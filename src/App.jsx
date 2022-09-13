9d2220b6c510fef8d2ff9a0f4389f2326ab1e85b
RokasAbra <rokasabramavicius@gmail.com>
2022-09-14 02:06:37 +0300

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Giphy from "./Components/Giphy";
+import "./App.css";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+const App = () => {
+
+  return (
+    <><BrowserRouter>
+    <Routes>
+      <Route path="/"
+      element={
+        <Giphy></Giphy>
+      }></Route>
+    </Routes>
+    </BrowserRouter>
+    {/* <div>
+      <Giphy />
+    </div> */}
+    </>
+    
+  );
+};
+
+
+
+export default App;
